0031f79a503e02acd0c2a033ba5da90110335b15
Nibofar <quentin.bouletbenac@gmail.com>
2021-03-11 15:15:37 +0100

    Génération un chemin infini

--- RuinLooter/Assets/Scripts/MapManager.cs
+++ RuinLooter/Assets/Scripts/MapManager.cs
@@ -16,18 +16,20 @@ public class MapManager : MonoBehaviour
             return _instance;
         }
     }
-    public GameObject spawnRoom;
     public List<Room> rooms = null;
+    public List<Room> first = null;
+    Room firstRoom;
     public static int count;
     void Awake()
     {
         _instance = this;
+        int random = Random.Range(0, first.Count);
         Vector2 spawnPos = transform.position;
         spawnPos.x = 0.0f;
         spawnPos.y = 0.0f;
-        spawnRoom = Instantiate(spawnRoom, spawnPos, Quaternion.identity);
+        firstRoom = Instantiate(first[random], spawnPos, Quaternion.identity);
         count = 0;
-        SpawnRoom(rooms[0]);
+        SpawnRoom(first[random]);
     }
 
     private void SpawnRoom(Room room)
