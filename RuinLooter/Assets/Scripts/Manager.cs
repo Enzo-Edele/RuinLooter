51b62629f97b3c248e4bc8e637f397a344ac86f7
Enzo-Edele <78355035+Enzo-Edele@users.noreply.github.com>
2021-04-20 14:17:49 +0200

    Test Build 3

--- RuinLooter/Assets/Scripts/Manager.cs
+++ RuinLooter/Assets/Scripts/Manager.cs
@@ -10,13 +10,6 @@ public class Manager : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
         UIManager.Instance.MainMenuButton();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-    private void Update()
-    {
-        if(SceneManager.GetActiveScene().buildIndex == 0)
-        {
-            UIManager.Instance.MainMenuButton();
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+        DontDestroyOnLoad(this.gameObject);
     }
 }
